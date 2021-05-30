10c37eb2a8b7e9beabe55660be5562358342218f
Ishika Kulkarni <44474766+Ishika13@users.noreply.github.com>
2021-05-30 09:25:53 +0530

    Add files via upload

---
+++ Characters and ASCII Values.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   char a;
+   printf("Enter a character: ");
+   scanf("%c", &a);
+   printf("You entered %c. \n" , a);
+   printf("ASCII value is %d." , a);
+   return 0;
+   }
