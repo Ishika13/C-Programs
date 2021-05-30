ae994c890e7d68b4115c914120afc08fefc52e88
Ishika Kulkarni <44474766+Ishika13@users.noreply.github.com>
2021-05-30 09:25:11 +0530

    Add files via upload

---
+++ Call by Value.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a = 15;
+    int b = 20;
+
+    int c;
+
+    swap(a,b)
+}
+
+viod swap(int a, int b)
+{
+
+    a = b;
+    a = c;
+
+    printf("%d,%d" , &a,&b);
+
+}
+
+
