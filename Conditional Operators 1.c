4871e8ad7bfc2031b2c55ce38613e3f5846cf192
Ishika Kulkarni <44474766+Ishika13@users.noreply.github.com>
2021-05-30 09:26:37 +0530

    Add files via upload

---
+++ Conditional Operators 1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a = 2;
+    int b = 5;
+
+    printf("Result %d\n" , a==b);
+    printf("Result %d\n" , a>b);
+    printf("Result %d\n" , a<b);
+
+    return 0;
+}
