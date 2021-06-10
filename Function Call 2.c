d718f3d36134f25702ff1fb197b67290c3fa43c6
Ishika Kulkarni <44474766+Ishika13@users.noreply.github.com>
2021-06-10 21:58:06 +0530

    Add files via upload

---
+++ Function Call 2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void add(int *a , *b , c)
+{
+    c = *a + *b;
+}
+
+int main()
+{
+ printf("a = ");
+ scanf("%d" , &a);
+ printf("b = ");
+ scanf("%d" , &b);
+ add(&c,c)
+ printf("Add : " ,add );
+ return 0;
+}
