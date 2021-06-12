8a90c1fbf4b5f68d369bacd955fb34fa2c80735f
Ishika Kulkarni <44474766+Ishika13@users.noreply.github.com>
2021-06-12 21:52:10 +0530

    Add files via upload

---
+++ Pointer Structure.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct student
+{
+    int id;
+    char name[50];
+    int p;
+};
+
+int main()
+{
+    int i;
+    struct student rec1 = {1,"Raju" , 90.5};
+    struct student *ptr;
+    prt = &rec1;
+
+}
