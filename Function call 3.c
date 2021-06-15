4afe0154628889a8de60442238cbc81d75ec993d
Ishika Kulkarni <44474766+Ishika13@users.noreply.github.com>
2021-06-15 22:24:24 +0530

    Add files via upload

---
+++ Function call 3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void disp (int *num)
+{
+    printf("%d" , *num);
+}
+
+int main()
+{
+    int a[];
+    for (int i = 0;i<10:i++)
+    {
+        disp (&arr[i]);
+    }
+    return 0;
+}
